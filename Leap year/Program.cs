6ace02d106371514437684eacde0d0140800dad6
vilko <helgi8@mail.ru>
2018-10-12 19:34:55 +0300

    Добавьте файлы проекта.

---
+++ Leap year/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap_year
+{
+    class Program
+    {
+        public static bool IsLeapYear(int year)
+        {
+            return DateTime.IsLeapYear(year);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsLeapYear(2014));
+            Console.WriteLine(IsLeapYear(1999));
+            Console.WriteLine(IsLeapYear(8992));
+            Console.WriteLine(IsLeapYear(1));
+            Console.WriteLine(IsLeapYear(14));
+            Console.WriteLine(IsLeapYear(400));
+            Console.WriteLine(IsLeapYear(600));
+            Console.WriteLine(IsLeapYear(3200));
+        }
+    }
+}
